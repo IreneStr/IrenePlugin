58b68e9f905d0464ddc7495e923f23732fa4c848
IreneStr <irene@yoast.com>
2017-01-03 15:26:09 +0100

    Add IrenePlugin

---
+++ IrenePlugin.php
@@ -0,0 +1,17 @@
+<?php
+
+
+/**
+ * Plugin Name: Irene in ALL CAPS
+ * Version: 0.1
+ * Description: The first plugin that changes 'Irene' to 'IRENE'.
+ * Author: IRENE
+ * License: GPL v3
+ */
+
+    add_filter('the_content', 'changeIrenetoIRENE');
+
+    function changeIrenetoIRENE($content)
+    {
+        return $content = str_replace("Irene", "IRENE", $content);
+    }
